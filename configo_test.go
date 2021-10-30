ddf19d00a4f7d27bba89907c3ae3004531d98aec
Affan Shahid <affan.shahid.94@gmail.com>
2021-10-30 09:50:11 +0500

    Add package example

--- configo_test.go
+++ configo_test.go
@@ -1,12 +1,53 @@
 package configo_test
 
 import (
+	"fmt"
+	"os"
 	"testing"
 	"testing/fstest"
 
 	"github.com/affanshahid/configo"
 	"github.com/stretchr/testify/assert"
 )
+
+func Example() {
+	dir := fstest.MapFS{
+		"default.yml": {
+			Data: []byte(`
+                root:
+                  prop1: foo
+                  prop2: 100
+                  prop3: false
+                  prop4:
+                    nestedProp1: 4
+            `),
+		},
+		"production.yml": {
+			Data: []byte(`
+                root:
+                  prop2: 200
+            `),
+		},
+	}
+
+	os.Setenv("APP_ENV", "production")
+
+	err := configo.Initialize(dir, configo.WithDeploymentFromEnv("APP_ENV"))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(configo.MustGetString("root.prop1"))
+	fmt.Println(configo.MustGetInt("root.prop2"))
+	fmt.Println(configo.MustGetBool("root.prop3"))
+	fmt.Println(configo.MustGetInt("root.prop4.nestedProp1"))
+
+	// Output:
+	// foo
+	// 200
+	// false
+	// 4
+}
 
 func TestGlobalConfig(t *testing.T) {
 	dir := fstest.MapFS{
